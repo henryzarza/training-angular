a3d952c9284c90e2d56ce4ea9660ad4216493aed
Henry Zarza <henryacz7@gmail.com>
2020-04-10 19:12:02 -0500

    Start with validation

--- src/app/screens/Login/login.component.ts
+++ src/app/screens/Login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  isLoginVisible = true;
+  form: FormGroup;
 
-  constructor() { }
+  ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,3}$')]),
+      password: new FormControl('', [Validators.required, Validators.minLength(5)])
+    });
+  }
 
-  ngOnInit(): void {
+  onSubmit() {
+    console.log(this.form.value);
+  }
+
+  get email() { return this.form.get('email'); }
+
+  toggleContent() {
+    this.isLoginVisible = !this.isLoginVisible;
+    if (this.isLoginVisible) {
+      this.form = new FormGroup({
+        email: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,3}$')]),
+        password: new FormControl('', [Validators.required, Validators.minLength(5)])
+      });
+    } else {
+      this.form = new FormGroup({
+        firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        locale: new FormControl('en', Validators.required),
+        email: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,3}$')]),
+        password: new FormControl('',
+          [Validators.required, Validators.minLength(5), Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]')]
+        ),
+        passwordConfirmation: new FormControl('', Validators.required)
+      }, this.passwordMatchValidator);
+    }
+  }
+
+  passwordMatchValidator(g: FormGroup) {
+    return g.get('password').value === g.get('passwordConfirmation').value ? null : { 'mismatch': true };
   }
 
 }
