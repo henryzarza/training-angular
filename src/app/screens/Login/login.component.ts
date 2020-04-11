25b40ce138d6407dd4ae8c6fea0fd51b0cd9e9ca
Henry Zarza <henryacz7@gmail.com>
2020-04-11 15:31:04 -0500

    Form validations

--- src/app/screens/Login/login.component.ts
+++ src/app/screens/Login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -13,22 +13,20 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
     this.form = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,3}$')]),
-      password: new FormControl('', [Validators.required, Validators.minLength(5)])
+      password: new FormControl('', [Validators.required, Validators.minLength(8)])
     });
   }
 
   onSubmit() {
-    console.log(this.form.value);
+    console.table(this.form.value);
   }
-
-  get email() { return this.form.get('email'); }
 
   toggleContent() {
     this.isLoginVisible = !this.isLoginVisible;
     if (this.isLoginVisible) {
       this.form = new FormGroup({
         email: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,3}$')]),
-        password: new FormControl('', [Validators.required, Validators.minLength(5)])
+        password: new FormControl('', [Validators.required, Validators.minLength(8)])
       });
     } else {
       this.form = new FormGroup({
@@ -36,16 +34,25 @@ export class LoginComponent implements OnInit {
         lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
         locale: new FormControl('en', Validators.required),
         email: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,3}$')]),
-        password: new FormControl('',
-          [Validators.required, Validators.minLength(5), Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]')]
-        ),
-        passwordConfirmation: new FormControl('', Validators.required)
-      }, this.passwordMatchValidator);
+        password: new FormControl('', [
+          Validators.required, Validators.minLength(8), this.passwordValidator(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]/)
+        ]),
+        passwordConfirmation: new FormControl('', [Validators.required, this.passwordMatchValidator()])
+      });
     }
   }
 
-  passwordMatchValidator(g: FormGroup) {
-    return g.get('password').value === g.get('passwordConfirmation').value ? null : { 'mismatch': true };
+  passwordMatchValidator() {
+    return (control: AbstractControl): {[key: string]: any} | null => {
+      return control.value === this.form.get('password').value ? null : { 'mismatch': true }
+    };
+  }
+
+  passwordValidator(expression: RegExp) {
+    return (control: AbstractControl): {[key: string]: any} | null => {
+      const isPasswordValid = expression.test(control.value);
+      return isPasswordValid ? null : { 'password': true };
+    };
   }
 
 }
