4f7a6f4c49fb8d1dec3d50c6a8d1a0d0a1d9a45c
Henry Zarza <henryacz7@gmail.com>
2020-04-11 19:17:32 -0500

    Home structure and card component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './screens/Login/login.component';
+import { HomeComponent } from './screens/home/home.component';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'login',
     component: LoginComponent
+  },
+  {
+    path: '',
+    component: HomeComponent
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GeneralComponentsModule } from './components/general-components.module';
 import { LoginComponent } from './screens/Login/login.component';
+import { HomeComponent } from './screens/home/home.component';
+import { HomeModule } from './screens/Home/home.module';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
-  imports: [BrowserModule, AppRoutingModule, GeneralComponentsModule, ReactiveFormsModule],
+  declarations: [AppComponent, LoginComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, GeneralComponentsModule, ReactiveFormsModule, HomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
