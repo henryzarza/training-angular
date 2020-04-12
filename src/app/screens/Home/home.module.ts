3efc035999fd8a60bd5941f2e2b6c986f33e060e
Henry Zarza <henryacz7@gmail.com>
2020-04-12 11:11:09 -0500

    Navbar component

--- src/app/screens/Home/home.module.ts
+++ src/app/screens/Home/home.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookCardComponent } from './components/book-card/book-card.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [BookCardComponent],
-  exports: [BookCardComponent],
+  declarations: [BookCardComponent, NavbarComponent],
+  exports: [BookCardComponent, NavbarComponent],
   imports: [CommonModule]
 })
 export class HomeModule { }
