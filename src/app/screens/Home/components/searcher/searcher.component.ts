d8ec6306463639d2f688d380cefefd3186e13637
Henry Zarza <henryacz7@gmail.com>
2020-04-13 17:12:53 -0500

    Searcher layout

---
+++ src/app/screens/Home/components/searcher/searcher.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-searcher',
+  templateUrl: './searcher.component.html',
+  styleUrls: ['./searcher.component.scss']
+})
+export class SearcherComponent {
+}
--- src/app/screens/Home/home.module.ts
+++ src/app/screens/Home/home.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookCardComponent } from './components/book-card/book-card.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearcherComponent } from './components/searcher/searcher.component';
 
 @NgModule({
-  declarations: [BookCardComponent, NavbarComponent],
-  exports: [BookCardComponent, NavbarComponent],
+  declarations: [BookCardComponent, NavbarComponent, SearcherComponent],
+  exports: [BookCardComponent, NavbarComponent, SearcherComponent],
   imports: [CommonModule]
 })
 export class HomeModule { }
