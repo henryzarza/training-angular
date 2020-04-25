2694dfc5738601ce9d03c5a211d090b9923b25fb
Henry Zarza <henryacz7@gmail.com>
2020-04-25 14:03:34 -0500

    Loader component

--- src/app/components/general-components.module.ts
+++ src/app/components/general-components.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { InputComponent } from './input/input.component';
+import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
-  declarations: [InputComponent],
-  exports: [InputComponent],
+  declarations: [InputComponent, LoaderComponent],
+  exports: [InputComponent, LoaderComponent],
   imports: [CommonModule, FormsModule],
 })
 export class GeneralComponentsModule {}
