0e55752b9dfefa0eeafd8f5ef00b72e53ee853f6
Henry Zarza <henryacz7@gmail.com>
2020-05-02 16:59:27 -0500

    Modal component

---
+++ src/app/components/modal/modal.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss']
+})
+export class ModalComponent {
+  @Input() headerTitle: string;
+  @Input() isOpen: boolean;
+  @Output() toggleModal = new EventEmitter();
+}
