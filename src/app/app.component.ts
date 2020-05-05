15579d64d09a5d75f726194c5ee2dcb30f85f001
Henry Zarza <henryacz7@gmail.com>
2020-05-05 18:20:12 -0500

    Starter files i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(translate: TranslateService) {
+    translate.addLangs(['en', 'klingon']);
+    translate.setDefaultLang('en');
+    translate.use('en');
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,7 +28,8 @@ import { environment } from 'src/environments/environment';
     AppRoutingModule,
     StoreModule.forRoot(appReducers),
     EffectsModule.forRoot([BooksEffects]),
-    environment.production ? [] : StoreDevtoolsModule.instrument()
+    environment.production ? [] : StoreDevtoolsModule.instrument(),
+    TranslateModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent],
